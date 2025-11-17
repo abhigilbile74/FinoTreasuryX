13b5d8d8e20e21562a0a83c448c07e744cf5943e
abhi <abhigilbile74@gmail.com>
2025-11-17 22:48:10 +0530

    create superuser

--- build.sh
+++ build.sh
@@ -5,3 +5,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+if [ "$CREATE_SUPERUSER" = "true" ]; then
+    python manage.py createsuperuser --no-input
+fi
