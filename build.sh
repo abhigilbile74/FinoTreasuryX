11e8344a66c004705bcb3ebcc389f1b710175c52
abhi <abhigilbile74@gmail.com>
2025-11-19 00:43:25 +0530

    bulid.sh update

--- build.sh
+++ build.sh
@@ -1,20 +1,29 @@
-#!/bin/bash
-set -o errexit
-set -o pipefail
+# #!/bin/bash
+# set -o errexit
+# set -o pipefail
 
-echo "📦 Installing dependencies..."
+# echo "📦 Installing dependencies..."
+# pip install -r requirements.txt
+
+# echo "🗂 Collecting static files..."
+# # Comment this to skip static collection if not needed
+# python manage.py collectstatic --no-input --clear
+
+# echo "🛠 Applying migrations..."
+# python manage.py migrate --no-input
+
+# echo "🚀 Starting Gunicorn (FAST MODE)..."
+# gunicorn Finance.asgi:application \
+#   -k uvicorn.workers.UvicornWorker \
+#   --bind 0.0.0.0:$PORT \
+#   --workers 1 \
+#   --timeout 0
+
+
+set -o errexit
+
 pip install -r requirements.txt
 
-echo "🗂 Collecting static files..."
-# Comment this to skip static collection if not needed
-python manage.py collectstatic --no-input --clear
+python manage.py collectstatic --no-input
 
-echo "🛠 Applying migrations..."
-python manage.py migrate --no-input
-
-echo "🚀 Starting Gunicorn (FAST MODE)..."
-gunicorn Finance.asgi:application \
-  -k uvicorn.workers.UvicornWorker \
-  --bind 0.0.0.0:$PORT \
-  --workers 1 \
-  --timeout 0
+python manage.py migrate
